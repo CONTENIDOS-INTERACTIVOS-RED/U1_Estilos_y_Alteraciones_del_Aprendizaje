131b759a65f75369fb8333926f95a0f14fddda29
Francisco Lizcano <flizcanor@icloud.com>
2025-02-14 07:37:58 -0500

    Update Introduccion.vue

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -1,18 +1,18 @@
 <template lang="pug">
 .curso-main-container.introduccion
   BannerInterno(subTitulo="Introducción")
-  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+  .container.tarjeta.tarjeta--blanca.px-4.px-md-5.pt-md-5.pt-4
     .row.mb-5
-      .col-lg-7
+      .col-lg-8
 
         p(data-aos="fade-right").mb-4 Los #[b trastornos específicos del aprendizaje (TEA)] son alteraciones del neurodesarrollo que afectan habilidades académicas como la lectura, la escritura y el cálculo matemático, a pesar de que las personas que los presentan tienen una inteligencia promedio o superior. Estos trastornos, entre los que se incluyen la dislexia, la disgrafía, la disortografía y la discalculia, dificultan el desempeño escolar y pueden generar problemas emocionales y de autoestima. Identificar y comprender estos trastornos es fundamental para desarrollar estrategias pedagógicas que permitan una enseñanza más inclusiva.
 
-        .bg-color-1.p-4(data-aos="fade-left").mb-4
+        .bg-color-1.p-4(data-aos="fade-left")
           p.mb-0 El origen de los TEA es multifactorial, con una base biológica influenciada por factores genéticos, epigenéticos y ambientales. La interacción de estos elementos puede afectar el procesamiento de la información verbal y no verbal, lo que impacta la capacidad de los estudiantes para adquirir y consolidar conocimientos. Aunque estos trastornos no están relacionados con la falta de esfuerzo o de capacidad intelectual, su detección tardía puede generar dificultades en el aprendizaje y en la adaptación al entorno escolar y social.
         
 
 
-      .col-lg-5
+      .col-lg-4
         figure
           img(src="@/assets/curso/intro/1.png", data-aos="zoom-in")
 
